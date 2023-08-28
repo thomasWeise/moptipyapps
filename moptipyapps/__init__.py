07f82f330cf2a177d24c0a8b3628a36528de907b
Thomas Weise <tweise@hfuu.edu.cn>
2023-08-29 07:41:44 +0800

    fixed strange issue when doing `pip --no-input --timeout 360 --retries 100 -v install .`

--- moptipyapps/__init__.py
+++ moptipyapps/__init__.py
@@ -14,6 +14,22 @@ The following additional tools are implemented:
 - :mod:`~moptipyapps.shared` offers shared constants and tools.
 """
 
-from moptipy.utils.sys_info import add_dependency
+# During some local pip installs, the other already installed packages may not
+# be accessible. This may cause
+# `from moptipy.utils.sys_info import add_dependency` to fail during
+# `pip3 --no-input --timeout 360 --retries 100 -v install .`.
+# The reason is not clear to me.
+# To prevent this issue, we now try to load the `moptipy` module and catch the
+# corresponding `ModuleNotFoundError` exception. Only if the module is loaded
+# correctly, we add `moptipyapps` to the dependencies.
+# Of course, this ignores the situation where there would really be an error.
+# But that is OK, because in that case we will crash later anyway.
 
-add_dependency("moptipyapps", ignore_if_make_build=True)
+can_do: bool = True
+try:
+    from moptipy.utils.sys_info import add_dependency
+except ModuleNotFoundError:
+    can_do = False
+
+if can_do:
+    add_dependency("moptipyapps", ignore_if_make_build=True)
